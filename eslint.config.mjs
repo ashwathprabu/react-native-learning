bcd13931562c39532e00f1b9c71cb85ebdee1e1f
ashwathprabu <prabhuashwath744@gmail.com>
2026-01-11 17:12:48 +0530

    completed eslint setup

---
+++ eslint.config.mjs
@@ -0,0 +1,29 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+	{
+		files: ["**/*.{js,mjs,cjs,jsx}"],
+		plugins: { js },
+		extends: ["js/recommended"],
+		languageOptions: { globals: globals.browser },
+		settings: {
+			react: {
+				version: "detect",
+			},
+		},
+	},
+	{
+		files: [
+			"metro.config.js",
+			"babel.config.js",
+			"jest.config.js",
+		],
+		languageOptions: {
+			globals: globals.node,
+		},
+	},
+	pluginReact.configs.flat.recommended,
+]);
